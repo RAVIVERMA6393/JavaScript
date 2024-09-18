4423b9b1ef2b3a2e3d341f820ab7fdd4afbbcb6d
RAVIVERMA6393 <147376604+RAVIVERMA6393@users.noreply.github.com>
2024-09-18 05:52:49 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use Strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log("Ravi");
+
+
+let name = "ravi"
+let age = 22
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigInt => 
+// srtring => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+
