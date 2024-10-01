116d35da70e14abbded49930ae83f7309b274ba5
RAVIVERMA6393 <147376604+RAVIVERMA6393@users.noreply.github.com>
2024-10-01 15:01:21 +0000

    controlflow

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,31 @@
+// //  Switch
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3    // string h to no. ki jagah string hoga
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 2:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case matched");
+        
+        break;
+}
